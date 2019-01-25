ee65a1373da6a654ed2cdefbf61bdc352ccf5ace
karlkorp <lispgod@gmail.com>
2019-01-25 19:43:38 +0200

    push

---
+++ get_next_line.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   gnl.h                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: arudyi <arudyi@student.unit.ua>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/11/05 18:36:41 by arudyi            #+#    #+#             */
+/*   Updated: 2018/12/07 18:39:49 by arudyi           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# define BUFF_SIZE 9999
+
+# include <stdlib.h>
+# include <unistd.h>
+# include <sys/types.h>
+# include <sys/stat.h>
+# include <fcntl.h>
+# include "libft.h"
+# include <limits.h>
+
+int	get_next_line(const int fd, char **line);
+#endif
